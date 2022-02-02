e9019fd607ec64183ff5fad47da81cb18937d497
James Pilkington <jpilkington332@gmail.com>
2022-02-02 07:46:35 -0700

    fixed news.api

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,15 +1,12 @@
+const axios = require("axios");
 require('dotenv').config();
-
-// console.log(require("dotenv").config())
-const { default: axios } = require("axios");
-//import 'dotenv/config'
-
 
 module.exports = async function() {
   try {
-    const response = await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=6a54a43d575c4d44a0e117eb032413d1&pageSize=5');
+    const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
     return response.data;
   } catch (error) {
     console.error(error);
   }
-}
+
+};
